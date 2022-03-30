38fc6be33afde2e32a06be76a7e02976ee8be88d
Yeremia Victor R <54694451+yeremiavictor@users.noreply.github.com>
2022-03-30 14:39:38 +0700

    isi dengan Api Anda

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,14 +5,14 @@ import {getAuth} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration (isi dengan api firebase Anda)
 const firebaseConfig = {
-  apiKey: "AIzaSyBmHMFjCzaECCz-aZWE_yjRYJuZB8ZtYG0",
-  authDomain: "projectauth-5d469.firebaseapp.com",
-  projectId: "projectauth-5d469",
-  storageBucket: "projectauth-5d469.appspot.com",
-  messagingSenderId: "1099247640281",
-  appId: "1:1099247640281:web:ee085fca84a642d22768c8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
